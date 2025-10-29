37c61c152b37a524494337a3d09fc51a25874f24
Abhi290703 <abhigec6@gmail.com>
2025-10-29 16:36:42 +0530

    29oct

--- 29oct.sql
+++ 29oct.sql
@@ -10,5 +10,27 @@
     emploc varchar(255),
     empgender char(1) check (empgender in ('M','F'))
   )
+
+select @@identity as currentvalue
+it checks the how many values are present their in the table
+
+select @@scope_identity()
+it also same has above but it checks only in the current scope
+
+  empid int identity (100,-1)
+  it gives the value from 100 and decrements by 1
+  example : 
+    empid   empname  emploc   empgender
+    100     abc      hyd       M
+    99      xyz      del       F        
+    98      pqr      mum       M
+ by selecting "select @@identity as currentvalue "
+ 
+
+
+
+
+
+
 */
 
