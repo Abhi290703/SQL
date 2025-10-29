52f82a860cc3db79963161a2563eacd4fcb29b70
Abhi290703 <abhigec6@gmail.com>
2025-10-29 16:24:57 +0530

    29oct

---
+++ 29oct.sql
@@ -0,0 +1,14 @@
+-- seed value / base value
+-- increment value
+
+/*
+  create table table_name(
+  empid int identity (1,1)
+                      | |
+                      seed increment
+    empname varchar (255),
+    emploc varchar(255),
+    empgender char(1) check (empgender in ('M','F'))
+  )
+*/
+
